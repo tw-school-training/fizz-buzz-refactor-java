203025189b608ad569c0f81d431e38ea3f574490
钟健鑫 <zhongjianxin@zjx.local>
2019-10-21 15:17:19 +0800

    [Jason]initial a bad smell practice on fizzbuzz requirement

---
+++ src/main/java/org/katas/refactoring/fizzbuzz/switchstatement/FizzBuzz.java
@@ -0,0 +1,49 @@
+package org.katas.refactoring.fizzbuzz.switchstatement;
+
+
+public class FizzBuzz {
+
+
+    public String sayIt(int input) {
+
+        String result = "";
+        String inputString = String.valueOf(input);
+        if (inputString.contains("3")) {
+            return "Fizz";
+        }
+        int flag = 0;
+        flag += input % 3 == 0 ? 0b100 : 0;
+        flag += input % 5 == 0 ? 0b010 : 0;
+        flag += input % 7 == 0 ? 0b001 : 0;
+
+        String[] flags = getBinaryString(flag).split("");
+        String[] items = {"Fizz", "Buzz", "Whizz"};
+
+        for (int i = 0; i < flags.length; i++) {
+            if(flags[i].equals("1")){
+                result += items[i];
+            }
+        }
+        if (!"".equals(result)) {
+            return result;
+        }
+        return String.valueOf(input);
+    }
+
+    private String getBinaryString(int flag) {
+        String flagString = Integer.toBinaryString(flag);
+        while (flagString.length() < 3) {
+            flagString = "0"+flagString;
+        }
+        return flagString;
+    }
+
+
+    public static void main(String[] args){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int i = 0; i < 200; i++) {
+            System.out.println(fizzBuzz.sayIt(i+1));
+        }
+
+    }
+}
---
+++ src/test/java/org/katas/refactoring/fizzbuzz/switchstatement/FizzBuzzTest.java
@@ -0,0 +1,24 @@
+package org.katas.refactoring.fizzbuzz.switchstatement;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+public class FizzBuzzTest {
+
+
+    @Test
+    public void should_return_normal_number_when_number_is_normal() {
+        int input = 1;
+        String expect = "1";
+        String actual = new FizzBuzz().sayIt(input);
+
+
+        assertThat(actual, Is.is(expect));
+    }
+
+
+
+
+}
